b9ada4210576bfb2ffeb40158046975f0a7f1d06
Chizim Cezar <chizimcezar@usa.com>
2022-09-29 16:30:45 +0300

    init

---
+++ website/templates/Base.tsx
@@ -0,0 +1,21 @@
+import { Meta } from '../layout/Meta';
+import Navbar from '../layout/Navbar';
+import { AppConfig } from '../utils/AppConfig';
+import { Footer } from './Footer';
+
+
+const Base = (props: any) => {
+
+  return(
+    <div className='antialiased bg-white text-gray-600 right-0 left-0'>
+      <Meta title={AppConfig.title} description={AppConfig.description} />
+      <Navbar />
+      <div className='mt-[4.5rem]'>
+        {props.children}
+      </div>
+      <Footer />
+    </div>
+  )
+};
+
+export { Base };
---
+++ website/utils/AppConfig.ts
@@ -0,0 +1,16 @@
+export const AppConfig = {
+  site_name: 'Finhubss',
+  title: 'Finhub Scaling Solution',
+  description: 'Finhub Scaling Solution',
+  locale: 'en',
+  email: 'info@finhubss.com',
+  adress: 'Beatrixgasse 27-2-15, 1030 Wien, Austria'
+};
+
+export const AppColors = {
+  black: '#040905',
+  dark_green: '#17271B',
+  green: '#25723E',
+  white: '#FFFFFF',
+  grey: '#EAF0EC'
+}
