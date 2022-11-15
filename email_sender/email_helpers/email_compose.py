8e9e9662778a6f4dcd9b07d36491c4b88282551d
Deffuls <deffuls@debian-BULLSEYE-live-builder-AMD64>
2022-11-15 18:35:45 +0200

    added two seprate functions for parsing request

--- email_sender/email_helpers/email_compose.py
+++ email_sender/email_helpers/email_compose.py
@@ -18,28 +18,40 @@ def create_file(f_bytes, file_name):
     f_obj['Content-Disposition'] = 'attachment; filename="%s"' % file_name
     return f_obj
 
-def h_compose_email(request_dict):
+def f_compose_email_cv(request_dict):
+    pass
+
+def f_compose_email_contact_us(request_dict):
     request_dict = request_dict.to_dict()
 
     _name = request_dict.get('name')
     _sender = request_dict.get('sender')
     _message = request_dict.get('message')
-    
-    _subject = "Received CV from %s" % _name
+    _company = request_dict.get('company')
+    _phone = request_dict.get('phone')
+
+    _root_content = "Company: %s" % _company
+    _root_content += "\nName: %s" % _name
+    _root_content += "\nPhone: %s" % _phone
+    _root_content += "\nEmail: %s" % _sender
+    _root_content += "\n\n"
+    _root_content += _message
+
+    _subject = "Contact us %s" % _company
 
     _composedMessage = MIMEMultipart()
 
     _composedMessage.preamble = _subject
     _composedMessage["Subject"] = _subject
-    _composedMessage["From"] = email_address
+    _composedMessage["From"] = _sender
     _composedMessage["To"] = email_address
 
-    _composedMessage.attach(MIMEText(_message, 'plain', 'utf-8'))
+    _composedMessage.attach(MIMEText(_root_content, 'plain', 'utf-8'))
     # _composedMessage.attach(create_file(_composedMessage[""]))
 
     return _composedMessage
 
-async def h_send_email_async(composed_message):
+async def f_send_email_async(composed_message):
     smtp_object = SMTP(hostname=smtp_host, port=smtp_port)
     await smtp_object.connect()
     await smtp_object.login(email_login, email_pass)
--- email_sender/flask_app.py
+++ email_sender/flask_app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from email_helpers.email_compose import h_compose_email, h_send_email_async
+from email_helpers.email_compose import f_compose_email_contact_us, f_send_email_async
 
 app = Flask(__name__)
 
@@ -9,11 +9,16 @@ app = Flask(__name__)
 async def index():
     return 'Hello world from flask!'
 
-@app.route('/api/email_sender', methods=['POST'])
-async def send_email_async():
+@app.route('/api/contact_us', methods=['POST'])
+async def handler_contact_us():
     _imutable = request.form
-    composed_email = h_compose_email(_imutable)
-    await h_send_email_async(composed_email)
+    composed_email = f_compose_email_contact_us(_imutable)
+    await f_send_email_async(composed_email)
+    return 'Success', 200
+
+@app.route('/api/send_cv')
+async def handler_send_cv():
+    _imutable = request.form
     return 'Success', 200
 
 if __name__ == "__main__":
